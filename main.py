3c1c0986b45e454663533e2fa06468a4ef12c653
Amanuel <amanuel.ayalew@aait.edu.et>
2025-08-29 21:12:37 +0300

    added normalization Layer

--- main.py
+++ main.py
@@ -27,3 +27,21 @@ class positionalEmbedding(nn.module):
       
         postion = torch.arrange(0,seq_len,d_model)
         div_term = torch.exp(torch.arange(0,d_model,2).float()*(-math.log(1000.0)/d_model))
+
+
+class LayerNormalization(nn.Module):
+
+    def __init__(self, features: int, eps:float=10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(features)) # alpha is a learnable parameter
+        self.bias = nn.Parameter(torch.zeros(features)) # bias is a learnable parameter
+
+    def forward(self, x):
+        # x: (batch, seq_len, hidden_size)
+         # Keep the dimension for broadcasting
+        mean = x.mean(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # Keep the dimension for broadcasting
+        std = x.std(dim = -1, keepdim = True) # (batch, seq_len, 1)
+        # eps is to prevent dividing by zero or when std is very small
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
